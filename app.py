0a24e12a8b69dbe034fe79536ee431967b67ef35
bhavani-github-user.com <priyain3114141@gmail.com>
2025-08-05 20:14:33 +0530

    Trigger test-Updated app message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Priya! Your Flask app is running on GCP 🚀"
+    return "CI/CD SUCCESS! Flask app deployed via Cloud Build ✅"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
